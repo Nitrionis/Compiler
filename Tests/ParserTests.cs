96b94b3d2c3a7028b129f4872ce6961a12562d19
Nitrionis <nitrodimon@mail.ru>
2020-01-21 16:47:37 +1000

    Add tests

--- Tests/ParserTests.cs
+++ Tests/ParserTests.cs
@@ -15,15 +15,7 @@ namespace ParserTests
 		[TestMethod] public void Test_5() => Run(5);
 		[TestMethod] public void Test_6() => Run(6);
 		[TestMethod] public void Test_7() => Run(7);
-		[TestMethod]
-		public void Test_8()
-		{
-			try {
-				Run(8);
-			} catch (ParserException e) {
-				Assert.IsTrue("Binary operation different types: int and float" == e.Message);
-			}
-		}
+		[TestMethod] public void Test_8() => Run(8);
 		[TestMethod] public void Test_9() => Run(9);
 		[TestMethod] public void Test_10() => Run(10);
 		[TestMethod] public void Test_11() => Run(11);
@@ -36,7 +28,6 @@ namespace ParserTests
 		[TestMethod] public void Test_18() => Run(18);
 		[TestMethod] public void Test_19() => Run(19);
 		[TestMethod] public void Test_20() => Run(20);
-		[TestMethod] public void Test_21() => Run(21);
 		[TestMethod] public void Test_22() => Run(22);
 		[TestMethod] public void Test_23() => Run(23);
 		[TestMethod] public void Test_24() => Run(24);
@@ -54,17 +45,35 @@ namespace ParserTests
 		[TestMethod] public void Test_36() => Run(36);
 		[TestMethod] public void Test_37() => Run(37);
 		[TestMethod] public void Test_38() => Run(38);
+		[TestMethod] public void Test_39() => Run(39);
+		[TestMethod] public void Test_40() => Run(40);
+		[TestMethod] public void Test_41() => Run(41);
+		[TestMethod] public void Test_42() => Run(42);
+		[TestMethod] public void Test_43() => Run(43);
+		[TestMethod] public void Test_44() => Run(44);
+		[TestMethod] public void Test_45() => Run(45);
+		[TestMethod] public void Test_46() => Run(46);
+		[TestMethod] public void Test_47() => Run(47);
 
 		private void Run(int testIndex)
 		{
-			var path = string.Format("ParserTests/ParserTest_{0}.txt", testIndex);
+			var path = string.Format("ParserTests/ParserTest_{0}_in.txt", testIndex);
 			Console.WriteLine("↓------------------Input----------------↓\n");
-			Console.WriteLine(File.ReadAllText(path));
+			var input = File.ReadAllText(path);
+			Console.WriteLine(input);
 			Console.WriteLine("\n↓------------------Output----------------↓\n");
 			var parser = new Parser.Parser(path);
-			foreach (var v in parser.ParseProgram()) {
-				Console.WriteLine(v?.ToString() ?? "null\n");
+			var output = "";
+			try {
+				foreach (var v in parser.ParseProgram()) {
+					output += v?.ToString() ?? "null\n";
+				}
+			} catch (ParserException e) {
+				output += e.Message;
 			}
+			Console.WriteLine(output);
+			var validOutput = File.ReadAllText(string.Format("ParserTests/ParserTest_{0}_out.txt", testIndex));
+			Assert.IsTrue(output.Equals(validOutput));
 		}
 	}
 }
